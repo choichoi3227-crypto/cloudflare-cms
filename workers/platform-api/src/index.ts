c6eebd74a623c8d75e8bf91b784022893aba714f
choichoi3227-crypto <choichoi3227@gmail.com>
2026-07-12 13:34:05 +0900

    Create index.ts

---
+++ workers/platform-api/src/index.ts
@@ -0,0 +1,4 @@
+// workers/platform-api/src/index.ts
+import type { Env } from './types';
+import { handleRequest } from './router';
+export default { async fetch(request: Request, env: Env): Promise<Response> { return handleRequest(request, env); } };
